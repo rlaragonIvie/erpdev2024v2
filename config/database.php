0f85ae2053c587ec2041e5b27f8bdeac12e288c1
rlaragonIvie <rodrigo.aragon@ivie.es>
2024-01-15 13:55:27 +0100

    Ajuste configuración oracle

--- config/database.php
+++ config/database.php
@@ -107,7 +107,6 @@ return [
             'edition' => env('DB_EDITION', 'ora$base'),
             'server_version' => env('DB_SERVER_VERSION', '11g'),
             'load_balance' => env('DB_LOAD_BALANCE', 'yes'),
-            'max_name_len' => env('ORA_MAX_NAME_LEN', 30),
             'dynamic' => [],
             'sessionVars' => [
                 'NLS_TIME_FORMAT' => 'HH24:MI:SS',
