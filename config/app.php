0bfd8d5474560ff02986472e71d54a90127e7ff3
rlaragonIvie <rodrigo.aragon@ivie.es>
2024-01-15 12:27:25 +0100

    instalado oracle

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
+        Yajra\Oci8\Oci8ServiceProvider::class, 
     ])->toArray(),
 
     /*
